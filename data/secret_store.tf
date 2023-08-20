3aa6e8ebc1151d9d6bba8d72c07f220c7bca612e
Vipul Sharda <vipulsharda153@gmail.com>
2023-08-20 10:31:28 +0530

    Create secret store and its policy

--- data/secret_store.tf
+++ data/secret_store.tf
@@ -24,7 +24,6 @@ resource "random_string" "bitbucket_password" {
 
 }
 
-
 resource "aws_secretsmanager_secret_version" "store_values" {
   depends_on    = [random_string.db_password, random_string.bitbucket_password, random_string.es_password]
   secret_id     = aws_secretsmanager_secret.store.id
@@ -42,4 +41,21 @@ resource "aws_secretsmanager_secret_version" "store_values" {
 EOF
 }
 
+data "aws_iam_policy_document" "example" {
+  version = "2012-10-17"
+  statement {
+    effect    = "Allow"
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["*"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_secretsmanager_secret_policy" "store_policy" {
+  secret_arn = aws_secretsmanager_secret.store.arn
+  policy     = data.aws_iam_policy_document.example.json
+}
 
