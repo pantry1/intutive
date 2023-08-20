ef3db24c355f3effb4ace096e4874d7046e79251
Vipul Sharda <vipulsharda153@gmail.com>
2023-08-20 14:28:51 +0530

    Create route53 entry for ELB for bitbucket

---
+++ atlassian/modules/bitbucket-module/route_53.tf
@@ -0,0 +1,16 @@
+data "aws_lb_hosted_zone_id" "alb" {}
+
+data "aws_lb" "bitbucket-alb" {
+  name = "bitbucket-alb"
+}
+
+#resource "aws_route53_record" "elb-route53" {
+#  name    = "scm.mhbeaws.gov"
+#  type    = "A"
+#  zone_id                = var.hosted_zone_id
+#  alias {
+#    name                   = aws_lb.bitbucket-alb.dns_name
+#    zone_id                = data.aws_lb_hosted_zone_id.alb.zone_id
+#    evaluate_target_health = true
+#  }
+#}
