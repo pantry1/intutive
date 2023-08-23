ea8e33cfdd3096caf7385d67bd9d6f123ab9cbe6
Vipul Sharda <vipulsharda153@gmail.com>
2023-08-23 21:56:25 +0530

    Use elb instead of lb

--- atlassian/modules/bitbucket-module/route_53.tf
+++ atlassian/modules/bitbucket-module/route_53.tf
@@ -1,6 +1,6 @@
 data "aws_lb_hosted_zone_id" "alb" {}
 
-data "aws_lb" "bitbucket-alb" {
+data "aws_elb" "bitbucket-alb" {
   name = "bitbucket-alb"
 }
 
@@ -9,8 +9,8 @@ data "aws_lb" "bitbucket-alb" {
 #  type    = "A"
 #  zone_id                = var.hosted_zone_id
 #  alias {
-#    name                   = aws_lb.bitbucket-alb.dns_name
-#    zone_id                = data.aws_lb_hosted_zone_id.alb.zone_id
+#    name                   = aws_elb.bitbucket-alb.dns_name
+#    zone_id                = data.aws_elb_hosted_zone_id.alb.zone_id
 #    evaluate_target_health = true
 #  }
 #}
