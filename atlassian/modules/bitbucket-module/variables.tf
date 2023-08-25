5cf2ad88153e00b13521ef6c62880c256e7a10fa
Vipul Sharda <vipulsharda153@gmail.com>
2023-08-25 21:18:27 +0530

    Organise variable file

--- atlassian/modules/bitbucket-module/variables.tf
+++ atlassian/modules/bitbucket-module/variables.tf
@@ -2,18 +2,6 @@ variable "cluster_name" {}
 
 variable "region" {}
 
-variable "rds_driver" {
-  default = "org.postgresql.Driver"
-}
-
-variable "nfs_mount_path" {
-  default = "/nfsshare"
-}
-
-variable "bitbucket_pods_count" {
-  default = 1
-}
-
 variable "private_subnets" {}
 
 variable "elb_type" {}
@@ -23,6 +11,7 @@ variable "eks_endpoint" {}
 variable "cluster_ca_certificate" {}
 
 variable "eks_token" {}
+
 variable "chart_status" {}
 
 variable "secret_store_name" {}
@@ -32,3 +21,11 @@ variable "secret_store_arn" {}
 variable "certificateARN" {}
 
 variable "hosted_zone_id" {}
+
+variable "rds_driver" {
+  default = "org.postgresql.Driver"
+}
+
+variable "bitbucket_pods_count" {
+  default = 1
+}
